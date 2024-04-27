0ba3d8f59ba1506cfbe5d5def25695de01550c20
Antoine Leclair <antoineleclair@gmail.com>
2024-04-26 20:48:37 -0400

    Add async DBSession

--- disco/config.py
+++ disco/config.py
@@ -1,2 +1,3 @@
 CADDY_VERSION = "2.7.6"
 SQLALCHEMY_DATABASE_URL = "sqlite:////disco/data/disco.sqlite3"
+SQLALCHEMY_ASYNC_DATABASE_URL = "sqlite+aiosqlite:////disco/data/disco.sqlite3"
--- disco/models/db.py
+++ disco/models/db.py
@@ -1,9 +1,10 @@
 import logging
 
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from disco.config import SQLALCHEMY_DATABASE_URL
+from disco.config import SQLALCHEMY_ASYNC_DATABASE_URL, SQLALCHEMY_DATABASE_URL
 
 log = logging.getLogger(__name__)
 
@@ -12,3 +13,9 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+async_engine = create_async_engine(
+    SQLALCHEMY_ASYNC_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+AsyncSession = async_sessionmaker(autocommit=False, autoflush=False, bind=async_engine)
