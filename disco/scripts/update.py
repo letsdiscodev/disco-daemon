f58ca50f36a7918c3f9c15cb5d2dfab0b5f39703
Antoine Leclair <antoineleclair@gmail.com>
2025-02-15 11:27:53 -0500

    Version 0.21.0

--- disco/scripts/update.py
+++ disco/scripts/update.py
@@ -137,6 +137,12 @@ def alembic_upgrade(version_hash: str) -> None:
     command.upgrade(config, version_hash)
 
 
+def task_0_20_x(image: str) -> None:
+    print("Updating from 0.20.x to 0.21.0")
+    with Session.begin() as dbsession:
+        keyvalues.set_value(dbsession=dbsession, key="DISCO_VERSION", value="0.21.0")
+
+
 def task_0_19_x(image: str) -> None:
     print("Updating from 0.19.x to 0.20.0")
     with Session.begin() as dbsession:
@@ -659,6 +665,8 @@ def get_update_function_for_version(version: str) -> Callable[[str], None]:
     if version.startswith("0.19."):
         return task_0_19_x
     if version.startswith("0.20."):
-        assert disco.__version__.startswith("0.20.")
+        return task_0_20_x
+    if version.startswith("0.21."):
+        assert disco.__version__.startswith("0.21.")
         return task_patch
     raise NotImplementedError(f"Update missing for version {version}")
