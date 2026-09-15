d929af17360e35e17b24feebbe3a0846bae2f596
Antoine Leclair <antoineleclair@gmail.com>
2026-09-15 12:48:16 -0400

    Use lock when naming nodes

--- disco/utils/nodes.py
+++ disco/utils/nodes.py
@@ -1,15 +1,21 @@
+import asyncio
+
 from disco.utils import docker
 from disco.utils.docker import NodeDetails
 from disco.utils.randomname import generate_random_name
 
+_naming_lock = asyncio.Lock()
+
 
 async def name_unnamed_nodes() -> list[NodeDetails]:
-    node_ids = await docker.get_node_list()
-    nodes = await docker.get_node_details(node_ids)
-    for node in nodes:
-        if "disco-name" not in node.labels:
-            node.labels["disco-name"] = await generate_random_name()
+    async with _naming_lock:
+        node_ids = await docker.get_node_list()
+        nodes = await docker.get_node_details(node_ids)
+        unnamed = [node for node in nodes if "disco-name" not in node.labels]
+        for node in unnamed:
             await docker.set_node_label(
-                node_id=node.id, key="disco-name", value=node.labels["disco-name"]
+                node_id=node.id, key="disco-name", value=await generate_random_name()
             )
-    return nodes
+        if len(unnamed) > 0:
+            nodes = await docker.get_node_details(node_ids)
+        return nodes
