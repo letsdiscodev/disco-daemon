fbede1d72d863e0032c61cbc36654accc874925e
Antoine Leclair <antoineleclair@gmail.com>
2026-09-15 12:48:38 -0400

    Remove redundant validation

--- disco/endpoints/deployments.py
+++ disco/endpoints/deployments.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from typing import Annotated
 
 from fastapi import APIRouter, BackgroundTasks, Depends, Header, HTTPException, Request
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field
 from sse_starlette import ServerSentEvent
 from sse_starlette.sse import EventSourceResponse
 
@@ -59,18 +59,6 @@ async def deployments_get(
 class DeploymentRequestBody(BaseModel):
     commit: str = Field("_DEPLOY_LATEST_", pattern=r"^\S+$")
     disco_file: DiscoFile | None = Field(None, alias="discoFile")
-
-    @model_validator(mode="after")
-    def commit_or_disco_file_required(self) -> "DeploymentRequestBody":
-        if self.commit is None and self.disco_file is None:
-            raise ValueError("Must provide one of commit or discoFile")
-        if (
-            self.commit is not None
-            and self.commit != "_DEPLOY_LATEST_"
-            and self.disco_file is not None
-        ):
-            raise ValueError("Must provide only one of commit or discoFile")
-        return self
 
 
 @router.post(
